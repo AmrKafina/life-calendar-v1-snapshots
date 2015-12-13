519dd67e18cd575e2a3656a1e53b2dee54f13660
Malfunction <Malfunction@Malfunctions-MacBook-Pro.local>
2015-12-13 11:22:10 +0300

    first servlet file

--- src/main/java/Snapshot.java
+++ src/main/java/Snapshot.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Snapshot extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
-    public DoubleMeServlet() {
+    public Snapshot() {
         super();
         
     }
