4a48361c15c321c571cd475e7471653b3fcaa554
Malfunction <Malfunction@Malfunctions-MacBook-Pro.local>
2016-01-24 12:54:17 +0300

    Notebooks v0.1

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -53,9 +53,30 @@ public class Notebook extends HttpServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-        PrintWriter out = response.getWriter();
-        out.println("NOTEBOOK IN YOUR FACE");
-
+        // Create a document and add a page to it
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage( page );
+        
+        // Create a new font object selecting one of the PDF base fonts
+        PDFont font = PDType1Font.HELVETICA_BOLD;
+        
+        // Start a new content stream which will "hold" the to be created content
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        
+        // Define a text content stream using the selected font, moving the cursor and drawing the text "Hello World"
+        contentStream.beginText();
+        contentStream.setFont( font, 12 );
+        contentStream.moveTextPositionByAmount( 100, 700 );
+        contentStream.drawString( "Hello World" );
+        contentStream.endText();
+        
+        // Make sure that the content stream is closed:
+        contentStream.close();
+        
+        // Save the results and ensure that the document is properly closed:
+        document.save( "Hello World.pdf");
+        document.close();
         
     }
     
