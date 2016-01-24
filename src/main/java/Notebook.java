ffeb9fd77ae93da82eecb9a8e78f5b643158fc61
Malfunction <Malfunction@Malfunctions-MacBook-Pro.local>
2016-01-24 12:55:39 +0300

    Notebooks v0.1

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -39,7 +39,7 @@ import java.awt.GraphicsEnvironment;
 import java.awt.FontFormatException;
 
 
-import org.apache.pdfbox.*;
+import org.apache.pdfbox;
 
 
 @WebServlet(name = "notebook",urlPatterns = {"/notebook/*"})
