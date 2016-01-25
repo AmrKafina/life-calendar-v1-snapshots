0c62de1e4e6832ffed2e8bac0558caede3f64cf4
Malfunction <Malfunction@Malfunctions-MacBook-Pro.local>
2016-01-25 13:03:07 +0300

    Notebooks v0.1

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -9,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletOutputStream;
+
 
 import java.awt.image.BufferedImage;
 import java.awt.Image;
