674884b8dabcc7938fd6f4e6de96a826ca07076a
Malfunction <Malfunction@Malfunctions-MacBook-Pro.local>
2016-01-26 11:12:32 +0300

    Notebooks v0.1

--- src/main/java/Notebook.java
+++ src/main/java/Notebook.java
@@ -49,6 +49,7 @@ import org.apache.pdfbox.pdmodel.*;
 import org.apache.pdfbox.pdmodel.font.*;
 import org.apache.pdfbox.pdmodel.edit.*;
 import org.apache.pdfbox.exceptions.*;
+import org.apache.pdfbox.pdmodel.common.*;
 
 
 
@@ -319,4 +320,82 @@ class Paragraph {
         return fontSize;
     }
     
+}
+
+class TextPdfElement implements PdfElement {
+    public enum TextAlignment {
+        CENTER,
+        LEFT,
+        RIGHT
+    };
+    
+    private String message;
+    private PDFont font;
+    private float fontSize;
+    private Color fontcolor;
+    private float x;
+    private float y;
+    private TextAlignment align = TextAlignment.LEFT;
+    
+    public TextPdfElement() {}
+    
+    public TextPdfElement( String message, PDFont font, float fontSize, Color fontColor, float x, float y, TextAlignment align ) {
+        this.message = message;
+        this.font = font;
+        this.fontSize = fontSize;
+        this.fontcolor = fontColor;
+        this.x = x;
+        this.y = y;
+        this.align = align;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public void setFont(PDFont font) {
+        this.font = font;
+    }
+    
+    public void setFontSize(float fontSize) {
+        this.fontSize = fontSize;
+    }
+    
+    public void setFontColor(Color fontColor) {
+        this.fontcolor = fontColor;
+    }
+    
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void setTextAlignment(TextAlignment align) {
+        this.align = align;
+    }
+    
+    @Override
+    public void draw(PDPageContentStream contentStream, PDRectangle region) {
+        if ( this.message == null)
+            return;
+        
+        try {
+            contentStream.beginText();
+            
+            contentStream.setFont( this.font, this.fontSize );
+            contentStream.setNonStrokingColor(this.fontcolor);
+            if (this.align == TextAlignment.CENTER) {
+                float stringWidth = font.getStringWidth( this.message )*fontSize/1000f;
+                float centerXPos = ( region.getWidth() - stringWidth ) / 2f;
+                contentStream.setTextTranslation(centerXPos, region.getHeight() -  (this.y*72) );
+            } else {
+                contentStream.setTextTranslation(this.x * 72, region.getHeight() - (this.y*72) );
+            }
+            
+            contentStream.drawString( this.message );
+            contentStream.endText();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
