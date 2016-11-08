0ee600b862aa4e92b3e792dced1339fec3153b5d
Amr Kafina <Amr@thapp.com>
2016-11-08 18:16:46 +0300

    chinese compatibility

--- src/main/java/SnapshotIOS.java
+++ src/main/java/SnapshotIOS.java
@@ -137,7 +137,7 @@ public class SnapshotIOS extends HttpServlet {
             int snapshotType = jRequest.getInt("type");
 
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().print("Sent title is " + snapshotTitle);
+            response.getWriter().print("Sent json is " + jsonString);
             response.getWriter().close();
 
             // creates an int array and reads the colors
